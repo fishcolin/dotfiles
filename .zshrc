62fba9717e279ec088ab3196e5327e730ee8591c
Colin <111797917+fishcolin@users.noreply.github.com>
2024-04-24 04:19:14 +0000

    Add basic dotfiles

---
+++ .zshrc
@@ -0,0 +1,8 @@
+# Set up Oh My Zsh
+export ZSH="$HOME/.oh-my-zsh"
+
+# Enable plugins (like git)
+plugins=(git)
+
+# Source Oh My Zsh to load the configurations
+source $ZSH/oh-my-zsh.sh
---
+++ install.sh
@@ -0,0 +1,17 @@
+# Install Zsh using the package manager for Ubuntu/Debian
+sudo apt install zsh
+
+# Install Oh My Zsh using the remote installation script
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Clone the Tig repository from GitHub (Tig is an ncurses-based text-mode interface for git)
+git clone git://github.com/jonas/tig.git
+cd tig && \
+  make && \           # Compile Tig from source
+  make install        # Install Tig binaries system-wide
+
+# Copy the local .zshrc file to the home directory to apply custom Zsh configurations
+cp .zshrc ~/.zshrc
+
+# Copy the local .gitconfig file to the home directory to apply custom Git configurations
+cp .gitconfig ~/.gitconfig
