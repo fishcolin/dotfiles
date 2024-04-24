49d81b91edc71fb745897863791d86e9792b3f7f
Colin <111797917+fishcolin@users.noreply.github.com>
2024-04-24 05:19:31 +0000

    Remove tig from dotfiles

--- install.sh
+++ install.sh
@@ -3,9 +3,3 @@ cp .zshrc ~/.zshrc
 
 # Copy the local .gitconfig file to the home directory to apply custom Git configurations
 cp .gitconfig ~/.gitconfig
-
-# Clone the Tig repository from GitHub (Tig is an ncurses-based text-mode interface for git)
-git clone git://github.com/jonas/tig.git
-cd tig && \
-  make && \           # Compile Tig from source
-  make install        # Install Tig binaries system-wide
