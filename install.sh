57c969f49531e3f74b4587945562e1cf76e98215
Colin <111797917+fishcolin@users.noreply.github.com>
2025-03-17 16:52:41 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 # Copy the local .zshrc file to the home directory to apply custom Zsh configurations
 cp .zshrc ~/.zshrc
 
+# mkdir and copy over .zsh.d contents
+mkdir ~/.zsh.d
+cp -r .zsh.d ~/.zsh.d
+
 # Copy the local .gitconfig file to the home directory to apply custom Git configurations
 cp .gitconfig ~/.gitconfig
