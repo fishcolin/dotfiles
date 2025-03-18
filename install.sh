8163147f9b5a7f149cd1d81f39fbc607f6c592e8
Colin <111797917+fishcolin@users.noreply.github.com>
2025-03-18 12:19:20 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,25 @@
-# Copy the local .zshrc file to the home directory to apply custom Zsh configurations
+#!/usr/bin/env bash
+
+# Install Homebrew if it's not already installed
+if ! command -v brew &>/dev/null; then
+  echo "Homebrew not found, installing Homebrew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# Run the Brewfile (assuming it's in the current directory)
+echo "Running Brewfile..."
+brew bundle --file=./.Brewfile
+
+# Apply custom Zsh configurations
+echo "Copying .zshrc to home directory..."
 cp .zshrc ~/.zshrc
 
-# mkdir and copy over .zsh.d contents
-mkdir ~/.zsh.d
-cp -r .zsh.d ~/.zsh.d
+echo "Creating ~/.zsh.d and copying contents..."
+mkdir -p ~/.zsh.d
+cp -r .zsh.d/* ~/.zsh.d
 
-# Copy the local .gitconfig file to the home directory to apply custom Git configurations
+# Apply custom Git configurations
+echo "Copying .gitconfig to home directory..."
 cp .gitconfig ~/.gitconfig
+
+echo "Installation complete."
